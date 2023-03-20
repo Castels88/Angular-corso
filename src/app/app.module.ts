2fca01948adf6a668b8d459375d140c6257d1db6
elio <elio.castellini@expleo.it>
2023-03-20 09:43:30 +0100

    fine lezione

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProvaComponent } from './prova/prova.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
+//come prima cosa andiamo ad implementare il componente di material all'interno della app.module
+//e poi dobbiamo aggiungere il componente anche negli import
+//adesso prendiamo l esempio di html che abbiamo nella pagina dei material design e andiamo nel html di prova =>
 @NgModule({
-  declarations: [
-    AppComponent,
-    ProvaComponent
-  ],
+  declarations: [AppComponent, ProvaComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
