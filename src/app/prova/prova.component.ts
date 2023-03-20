922adb9c0a85045bc17c0d359f22f8b68f3669f5
elio <elio.castellini@expleo.it>
2023-03-20 12:54:19 +0100

    fine lezione

--- src/app/prova/prova.component.ts
+++ src/app/prova/prova.component.ts
@@ -1,11 +1,60 @@
-import { Component, OnInit } from '@angular/core';
-
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+//adesso andiamo a vedere il ciclo di vita dei componenti
+//il primo step di vita del componente lo possiamo definire col ng OnInit{}
+//successivamente ngDoCheck{} che fara un controllo del componente appena creato
+//poi viene inizializzato il contenuto del componente
+//poi si ha il check del contenuto che e' stato inizializzato
+//succesivamente si ha un controllo della view della pagina
+//il componete dopo questi controlli rimane li fino a quando non lo distruggiamo
+// o decidiamo di cambiare la pagina
 @Component({
   selector: 'app-prova',
   templateUrl: './prova.component.html',
   styleUrls: ['./prova.component.css'],
 })
-export class ProvaComponent implements OnInit {
-  constructor() {}
-  ngOnInit(): void {}
+//andiamo ad implementare tutti i cicli di vita del componente
+//ci dara un errore perche dobbiamo implementarli nel TS
+export class ProvaComponent
+  implements
+    OnInit,
+    AfterContentChecked,
+    AfterContentInit,
+    AfterViewChecked,
+    AfterViewInit,
+    DoCheck,
+    OnDestroy
+{
+  constructor() {
+    console.log('costruttore');
+  }
+  ngAfterContentChecked(): void {
+    console.log('ngAfterContentChecked');
+  }
+  ngAfterContentInit(): void {
+    console.log('ngAfterContentInit.');
+  }
+  ngAfterViewChecked(): void {
+    console.log('ngAfterViewChecked');
+  }
+  ngAfterViewInit(): void {
+    console.log('ngAfterViewInit');
+  }
+  ngDoCheck(): void {
+    console.log('ngDoCheck');
+  }
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
+  }
+  ngOnInit(): void {
+    console.log('onInit');
+  }
 }
