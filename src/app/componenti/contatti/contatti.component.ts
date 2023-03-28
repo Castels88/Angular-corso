9712b37e2aa9cac64e26ce33b47765b7f67a50ff
elio <elio.castellini@expleo.it>
2023-03-28 14:29:02 +0200

    correzione

--- src/app/componenti/contatti/contatti.component.ts
+++ src/app/componenti/contatti/contatti.component.ts
@@ -14,24 +14,21 @@ import { ServizioProvaService } from './../../servizi/servizio-prova.service';
 //nome del parametro che vogliamo venga preso)'
 export class ContattiComponent implements OnInit {
   persone: any;
+  persona: any;
   isProfile: boolean = false;
   constructor(
     private servizioProva: ServizioProvaService,
     private route: ActivatedRoute
   ) {}
   ngOnInit(): void {
-    // this.persone = this.servizioProva.getPersone();
-    // this.isProfile = !this.route.snapshot.paramMap.get('id') ? false : true;
     if (this.route.snapshot.paramMap.get('id')) {
       this.isProfile = true;
-      this.persone = this.servizioProva.getPersona(
+      this.persona = this.servizioProva.getPersona(
         parseInt(this.route.snapshot.paramMap.get('id')!)
       );
     } else {
       this.isProfile = false;
       this.persone = this.servizioProva.getPersone();
     }
-    console.log(this.route.snapshot.paramMap.get('id'));
-    console.log(this.isProfile);
   }
 }
